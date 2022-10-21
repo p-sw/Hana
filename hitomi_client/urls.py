ac5423c4e7b66a2daf42ed5072ad78f571ff84b2
sserve-kr <personal@sserve.me>
2022-10-21 20:48:32 +0900

    Fixed URL bug

--- hitomi_client/urls.py
+++ hitomi_client/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/user', include('user.urls', namespace='user'))
+    path('user/', include('user.urls', namespace='user'))
 ]
