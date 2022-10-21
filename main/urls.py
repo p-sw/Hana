bfe80eaf09776a85dac62d2712c44315c1352fc4
sserve-kr <personal@sserve.me>
2022-10-22 01:24:27 +0900

    Added index page in main app

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 app_name = "main"
 
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+
+from . import views
 
 urlpatterns = [
-
+    path('', login_required(views.IndexView.as_view()), name='index'),
 ]
