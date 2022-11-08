5207ad07d652bee0121f872e929197cc80aef20a
sserve-kr <personal@sserve.me>
2022-11-08 18:14:44 +0900

    Fixed main url

--- hitomi_client/urls.py
+++ hitomi_client/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace='user')),
-    path('/', include('main.urls', namespace='main')),
+    path('', include('main.urls', namespace='main')),
     path('view/', include('viewer.urls', namespace='viewer')),
 ]
