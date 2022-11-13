a4c6b96a1e6915aa8480f9e2bd4a6d5744e5e698
Ubuntu <ubuntu@ip-172-26-3-24.ap-northeast-2.compute.internal>
2022-11-13 16:24:23 +0000

    Changed domains in production configuration

--- hitomi_client/settings/prod.py
+++ hitomi_client/settings/prod.py
@@ -5,5 +5,5 @@ SECRET_KEY = token_hex(50)
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["sserve.work"]
-CSRF_TRUSTED_ORIGINS = ["https://hc.sserve.work"]
+ALLOWED_HOSTS = ["hana.sserve.work"]
+CSRF_TRUSTED_ORIGINS = ["https://hana.sserve.work"]
