e87607b68026daab144ed6aeb023452fc2cf0f1a
SSerVe <personal@sserve.me>
2022-11-16 23:00:53 +0900

    Added django admin search

--- main/admin.py
+++ main/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Tag, Favorites
 
 # Register your models here.
-admin.site.register(Tag)
+
+
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['tagtype', 'name']
+
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Favorites)
