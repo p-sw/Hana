4b17ba15a6173a1ac93137d03322c450ae24d114
sserve-kr <personal@sserve.me>
2022-11-16 11:09:25 +0900

    Redesigned viewer navigation

--- viewer/static/viewer/js/nav.js
+++ viewer/static/viewer/js/nav.js
@@ -1,3 +1,35 @@
+class StarManager {
+    constructor() {
+        // get current favorite status
+        fetch('/api/favorite/get-favorite-by-id', {
+            method: 'GET',
+        }).then((response) => response.json()).then((data) => {
+            this.isFavorite = data["exists"];
+
+            this.star = document.querySelector("#star img");
+            this.star_normal = this.star.src;
+            this.star_active = this.star.dataset.src;
+
+            this.updateStar();
+        })
+    }
+
+    updateStar() {
+        if (this.isFavorite) {
+            this.star.src = this.star_active;
+        } else {
+            this.star.src = this.star_normal;
+        }
+    }
+
+    toggleStar() {
+        // toggle using server api
+
+        this.isFavorite = !this.isFavorite;
+        this.updateStar();
+    }
+}
+
 class Nav {
     constructor (galleryid) {
         this.nav = document.querySelector('nav');
@@ -7,9 +39,17 @@ class Nav {
             this.nav.querySelector("h1").innerText = /"title":"([^"]+)"/.exec(data)[1];
         });
 
-        this.nav.querySelector("img").addEventListener("click", () => {
+        this.nav.querySelector("#menu-open").addEventListener("click", this.openMenu.bind(this));
+        this.nav.querySelector("#menu-close").addEventListener("click", this.closeMenu.bind(this));
+        this.nav.querySelector('#back').addEventListener("click", () => {
             window.history.back();
         })
+        this.nav.querySelector("#home").addEventListener("click", () => {
+            window.location.href = "/";
+        })
+
+        this.starManager = new StarManager();
+        this.nav.querySelector("#star").addEventListener("click", this.starManager.toggleStar.bind(this.starManager));
     }
 
     getScript () {
@@ -17,5 +57,19 @@ class Nav {
             method: 'GET',
             cache: 'force-cache',
         }).then(response => response.text())
+    }
+
+    openMenu() {
+        this.nav.querySelector("#menu-open").style.display = "none";
+        this.nav.querySelector("#menu-close").style.display = "block";
+        this.nav.querySelector("#main-menu").classList.remove("closed");
+        this.nav.querySelector("#main-menu").classList.add("opened");
+    }
+
+    closeMenu() {
+        this.nav.querySelector("#menu-open").style.display = "block";
+        this.nav.querySelector("#menu-close").style.display = "none";
+        this.nav.querySelector("#main-menu").classList.add("closed");
+        this.nav.querySelector("#main-menu").classList.remove("opened");
     }
 }
