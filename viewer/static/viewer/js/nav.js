ff2a8ada8f8b9df23cd24ba293f56ff909d80aaa
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-11-19 19:15:45 +0900

    Removed title in viewer

--- viewer/static/viewer/js/nav.js
+++ viewer/static/viewer/js/nav.js
@@ -41,9 +41,9 @@ class Nav {
         this.nav = document.querySelector('nav');
         this.galleryid = galleryid;
 
-        this.getScript().then((data) => {
+        /* this.getScript().then((data) => {
             this.nav.querySelector("h1").innerText = /"title":"([^"]+)"/.exec(data)[1];
-        });
+        }); */
 
         this.nav.querySelector("#menu-open").addEventListener("click", this.openMenu.bind(this));
         this.nav.querySelector("#menu-close").addEventListener("click", this.closeMenu.bind(this));
